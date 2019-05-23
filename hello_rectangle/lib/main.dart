b3043f65ea8bc5d7fbcca020edcaf0adc328cf02
小針隼一郎 <>
2019-05-23 22:34:26 +0900

    ステートクラスとの連携、FloatingActionButtonの利用

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -16,7 +16,6 @@ void main() => runApp(new MyApp());
 //Stateless⇨静的な表示
 class MyApp extends StatelessWidget {
   final title = 'Flutterサンプル';
-  final message = 'サンプルメッセージ';
 
 /* ウィジェットが生成される時に呼び出されるメソッド
  * BuildContextは、組み込まれたウィジェットに関する機能
@@ -25,44 +24,53 @@ class MyApp extends StatelessWidget {
  */
   @override
   Widget build(BuildContext context) { 
-    /*
-     * material VS cupertino
-     * デフォルトで生成されるスクリプトでmaterial.dartが読み込まれるように
-     * 設定されていることで、明示的にcupertino.dartを読み込まない限りは
-     * マテリアルデザインベースでUIが構築される
-     */
-    /* MaterialAppインスタンスをreturnすることでマテリアルデザインによる
-     * アプリが表示されるようになる。
-     * 画面に表示されるウィジェットなどを引数に設定
-     */
+    
     return new MaterialApp(
       //アプリのタイトル
       title: 'Flutter Demo',
       //homeに,組み込まれるウィジェットを設定
-      /*
-       * Scaffoldは「足場」の意。アプリ作成の土台となる。
-       * マテリアルデザインの基本的なデザインとレイアウトが組み込まれている。
-       * これに必要なウィジェットを追加することで一般的なデザインのアプリが作成される。
-       */
       home: new MyHomePage(
         title: this.title,
-        message: this.message
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  final String message;
 
-  MyHomePage({this.title, this.message}): super();
+  MyHomePage({this.title}): super();
+  final String title;
   
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+//ステートクラス
 class _MyHomePageState extends State<MyHomePage> {
+  String _message;
+
+  //プロパティの初期化。Stateクラスに専用のメソッドとして用意されている。
+  /**
+   * initStateは、インスタンスが作成され、ウィジェットのツリー構造への
+   * 組み込みが完了した時点で呼び出される。
+   * コンストラクタではウィジェットツリーが完了していない為にエラーになるような
+   * 処理(他のウィジェットの参照など)もinitStateでは問題なく実行できる。
+   */
+  @override
+  void initState(){
+    super.initState();
+    _message = 'Hello!';
+  }
+
+  void _setMessage(){
+
+    //setStateメソッドはステートの更新をステートクラスに知らせる働きをする。
+    //このメソッドに、必要な値の変更処理を用意する。引数は関数。
+    setState(() {
+      _message = 'タップしました！';
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -70,8 +78,18 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       
       body: Text(
-        widget.message,
+        _message,
         style: TextStyle(fontSize: 32.0),
+      ),
+      //フローティングアクションボタン。丸いアイコンを表示したボタン。常に右下に表示される
+      //インスタンスを作成するには匿名クラスの要素としてこれらのプロパティを用意しておく。
+      floatingActionButton: FloatingActionButton(
+        //onPressed:ボタンをタップした時に呼び出されるメソッド
+        onPressed: _setMessage,
+        //tooltip:ツールチップとして表示するテキスト
+        tooltip: 'set message.',
+        //child:このウィジェット内に組み込まれているウィジェット類をまとめたもの
+        child: Icon(Icons.star),
       ),
     );
   }
