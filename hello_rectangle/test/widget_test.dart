8e8b6feff76daa30916dfb908c0b3cb3d5a8c3fb
小針隼一郎 <>
2019-05-28 23:34:53 +0900

    テストファイル、非同期処理

--- hello_rectangle/test/widget_test.dart
+++ hello_rectangle/test/widget_test.dart
@@ -11,6 +11,32 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_rectangle/main.dart';
 
 void main() {
+  /**
+   * testWidgets関数：引数に説明のテキストとコールバック関数を用意
+   * 関数内に実行するテストの内容を記述
+   */
+  /**
+   * WidgetTester：Flutterのウィジェットをテストするためのクラス。
+   * この引数で渡されるWidgetTester内から、テストに必要なメソッドなどを呼び出してテストを行う。
+   */
+  /**
+   * 【非同期処理】について
+   *  非同期処理：処理に時間がかかる場合、処理に終了を待たずに次の処理へと進む方式のこと
+   *  ❶非同期メソッドの定義
+   *    メソッド(引数) async {...}
+   *  ❷非同期メソッドの呼び出し
+   *   方法⑴
+   *    Future<?> 変数 = メソッド ();
+   *    [Future].then(...);
+   *  ・非同期メソッドでは、戻り値にFutureという、将来的な戻り値を予約するためのクラスのインスタンスが返される。
+   *  実行時にはまだ処理が完了していない⇨「将来、この戻り値を返します」ということを予約しておくための特殊な値を返す。
+   *  Future<String>は、将来Stringの値が返されるということ。
+   *  ・非同期の処理が完了し、値が返されると、Futureのthenメソッドが呼び出される。引数は通常、関数になっている。
+   *   非同期メソッドの戻り値がその関数に引数として渡される。この引数を使って必要な処理を行う。
+   *   方法⑵
+   *     await メソッド();
+   *    処理を完了するまで待つ、つまり、非同期処理を無理やり同期処理に変える。
+   */
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
